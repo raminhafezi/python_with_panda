993274597aaea77efe92b7884a4c28c0d66401da
raminhafezi@gmail.com <raminhafezi@gmail.com>
2018-07-26 17:54:07 +1000

    push from BAG Desktop

--- 03_video.sh
+++ 03_video.sh
@@ -5,8 +5,8 @@ import pandas as pd
 	We will cover Panda's IO in this code.
 '''
 #read from CSV
-file_address = '/home/ramin/Desktop/git___workspace/python_with_pandas/daily-leaderboard-2018-07-20.csv'
-
+#file_address = '/home/ramin/Desktop/git___workspace/python_with_pandas/daily-leaderboard-2018-07-20.csv'
+file_address = '/home/ramin/git_workspace/python_with_panda/daily-leaderboard-2018-07-20.csv'
 
 #put it into the dataFrame
 df = pd.read_csv(file_address)
@@ -19,11 +19,11 @@ df.to_csv('newFile.csv')
 #print(df.head())
 
 #make it a new dataFrame or basically re-design the dataFrame
-df = pd.read_csv('/home/ramin/Desktop/git___workspace/python_with_pandas/newFile.csv')
+#df = pd.read_csv('/home/ramin/Desktop/git___workspace/python_with_pandas/newFile.csv')
+df = pd.read_csv('/home/ramin/git_workspace/python_with_panda/newFile.csv')
 
 
 # we can also set the index at the time of reading the CSV file
-
 df = pd.read_csv(file_address, index_col = 'max_max_position_concentration')
 print(df.head(125))
 #print(df.columns.values)
